df6e744e0ff7e4c54e24837815a4e82c37dcc5e8
Imer Durgut <idurgut@gmail.com>
2024-11-14 16:25:27 +0100

    created new list

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,29 @@
 class ListsController < ApplicationController
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.all(set_list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def list_params
+    params.require(:list).permit(:list_id)
+  end
 end
